110b8505cfdb95f3894cff0b0b81c98445cc05f7
guopengfei <guopengfei@163.com>
2025-12-22 08:10:05 +0800

    1 增加todo

--- flask_app/services/llm_service.py
+++ flask_app/services/llm_service.py
@@ -106,7 +106,7 @@ class LLMService:
         """
         provider_config = self.config.LLM_PROVIDERS[provider_id]
         
-        # DeepSeek 从数据库读取
+        # DeepSeek 从数据库读取 TODO 这里要改为统一的env访问方式
         if provider_id == 'deepseek':
             from ..database import get_session
             from ..models import ApiKey
